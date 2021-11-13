65cf73a77ae3fda59abc1a45293e012675d33969
Mischa Spiegelmock <me@mish.dev>
2021-11-13 21:19:09 +0200

    generate merkle tree in test

---
+++ lib/proof.ts
@@ -0,0 +1,14 @@
+import { keccak256 } from "ethers/lib/utils";
+import { MerkleTree } from "merkletreejs";
+
+// convert hex string (0xabcd123) to hash hashing not the string but the actual value
+const hashStringAddress = (address: string): string =>
+  keccak256(Buffer.from(address.substring(2), "hex")); // decode hex string
+
+export function generateMerkleTree(addresses: string[]) {
+  const leaves = addresses.map((v: string) => hashStringAddress(v));
+  return new MerkleTree(leaves, undefined, { sort: true });
+}
+
+export const getProof = (tree: MerkleTree, address: string): string[] =>
+  tree.getHexProof(hashStringAddress(address));
