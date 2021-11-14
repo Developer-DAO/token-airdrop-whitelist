fd6c8cb82798eeabcc03d3c3af754e53e4d56d5c
Mischa Spiegelmock <me@mish.dev>
2021-11-14 10:29:58 +0200

    ganache, proofs

--- web/getProof.ts
+++ web/getProof.ts
@@ -1,5 +1,5 @@
 import * as proofs from "../proofs.json";
 
-export function getProof(address: keyof typeof proofs): string | undefined {
+export function getProof(address: keyof typeof proofs): string[] | undefined {
   return proofs[address];
 }
